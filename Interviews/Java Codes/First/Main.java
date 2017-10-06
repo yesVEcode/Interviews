6d9f9d85e5847afaff2ed85f4c9deee009ea1861
=yesVEcode <=yesvecode@gmail.com>
2017-10-06 13:35:38 +0300

    First

---
+++ Interviews/Java Codes/First/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    
+   
+    public static void main (String [] args) {
+        String first = "veysel vay nice ohoo";
+        String [] arr = first.split(" ");
+        
+        for(String item : arr )
+        {
+            System.out.println(item);
+        }
+    }
+
+}
